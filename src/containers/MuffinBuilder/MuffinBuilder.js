3e755a5a71be6bcbd9c689c0ca73d03257b6b21e
naveendk <naveenkolambage@gmail.com>
2019-02-05 20:50:30 +1200

    added components/containers and ingredientcss

---
+++ src/containers/MuffinBuilder/MuffinBuilder.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import Aux from '../../hoc/Auxiliary';
+
+class MuffinBuilder extends Component{
+    render(){
+        return(
+            <>
+              <div>Muffin</div>
+              <div>Base Controls</div>
+            </>
+        );
+    }
+}
+
+export default MuffinBuilder;
