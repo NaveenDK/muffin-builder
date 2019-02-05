135a393000f51faacf2589fab87f92020f74e97f
naveendk <naveenkolambage@gmail.com>
2019-02-06 11:53:34 +1200

    added the generic way of outputing ingredients

--- src/components/Muffin/Muffin.js
+++ src/components/Muffin/Muffin.js
@@ -3,11 +3,20 @@ import classes from './Muffin.css';
 import MuffinIngredient  from './MuffinIngredient/MuffinIngredient';
 
 const muffin = (props) =>{
+  
+  const transformedIngredients = Object.keys(props.ingredients)
+       .map(igKey => {
+           return[...Array(props.ingredients[igKey])].map((_,i)=>{
+              
+                return  <MuffinIngredient key={igKey+i} type={igKey}/>
+           });
+        
+       });
+       console.log(transformedIngredients)
     return(
         <div className={classes.Muffin}>
-          <MuffinIngredient type="bread-top"/>
-          <MuffinIngredient type="cheese"/>
-          <MuffinIngredient type="meat"/>
+          <MuffinIngredient  type="bread-top"/>
+          {transformedIngredients}
           <MuffinIngredient type="bread-bottom"/>
         </div>
     );
