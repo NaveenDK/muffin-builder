8be61f8525a74ce98f28ea6c8fb4b589ec4541a9
naveendk <naveenkolambage@gmail.com>
2019-02-06 14:57:51 +1200

    added safe remove functionality

--- src/containers/MuffinBuilder/MuffinBuilder.js
+++ src/containers/MuffinBuilder/MuffinBuilder.js
@@ -38,11 +38,39 @@ class MuffinBuilder extends Component{
         const newPrice=  oldPrice +priceAddition;
         this.setState({totalPrice: newPrice,ingredients:updatedIngredients});
     }
+   
+    removeIngredientHandler= (type) =>{
+        const oldCount = this.state.ingredients[type];
+        if(oldCount <=0){
+            return;
+        }
+        const updatedCount = oldCount-1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+        const priceDeduction = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - priceDeduction;
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients });
+    }
+ 
+
     render(){
+        const disabledInfo ={
+            ...this.state.ingredients
+        };
+        for ( let key in disabledInfo ){
+            disabledInfo[key] = disabledInfo[key] <=0
+        }
+
         return(
             <>
               <Muffin ingredients={this.state.ingredients}/>
-              <BaseControls ingredientAdded={this.addIngredientHandler}/>
+              <BaseControls ingredientAdded={this.addIngredientHandler}
+              ingredientRemoved={this.removeIngredientHandler}
+            disabled={disabledInfo}
+              />
             </>
         );
     }
