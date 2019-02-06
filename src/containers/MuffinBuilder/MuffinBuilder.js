23641751cd247766702565283566a46d9a250111
naveendk <naveenkolambage@gmail.com>
2019-02-06 16:45:34 +1200

    added the ordernow button

--- src/containers/MuffinBuilder/MuffinBuilder.js
+++ src/containers/MuffinBuilder/MuffinBuilder.js
@@ -24,8 +24,24 @@ class MuffinBuilder extends Component{
             cheese:0,
             meat:0
         },
-        totalPrice:4
+        totalPrice:4,
+        purchaseable: false
     }
+
+    updatePurchaseState(ingredients){
+
+   
+        const sum = Object.keys(ingredients)
+            .map(igKey=>{
+                   return ingredients[igKey] 
+            })
+            .reduce((sum, el)=>{
+                    return sum +el;
+            },0);
+        this.setState({purchasable: sum>0})
+    }
+
+
     addIngredientHandler = (type) =>{
         const oldCount = this.state.ingredients[type];
         const updatedCount = oldCount +1;
@@ -37,6 +53,7 @@ class MuffinBuilder extends Component{
         const oldPrice = this.state.totalPrice;
         const newPrice=  oldPrice +priceAddition;
         this.setState({totalPrice: newPrice,ingredients:updatedIngredients});
+        this.updatePurchaseState(updatedIngredients);
     }
    
     removeIngredientHandler= (type) =>{
@@ -53,6 +70,7 @@ class MuffinBuilder extends Component{
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice - priceDeduction;
         this.setState({totalPrice: newPrice, ingredients: updatedIngredients });
+        this.updatePurchaseState(updatedIngredients);
     }
  
 
@@ -68,9 +86,10 @@ class MuffinBuilder extends Component{
             <>
               <Muffin ingredients={this.state.ingredients}/>
               <BaseControls ingredientAdded={this.addIngredientHandler}
-              ingredientRemoved={this.removeIngredientHandler}
-            disabled={disabledInfo}
-              price ={this.state.totalPrice}
+                  ingredientRemoved={this.removeIngredientHandler}
+                 disabled={disabledInfo}
+                 purchasable ={this.state.purchasable}
+                 price ={this.state.totalPrice}
               />
             </>
         );
